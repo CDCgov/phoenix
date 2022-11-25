9d669969134901ee3b114b8a9b504bf159aa2528
Maria Diaz <69120361+MzDiaz824@users.noreply.github.com>
2022-11-25 14:33:40 -0500

    Create scaffolds_samplesheet.py

---
+++ bin/scaffolds_samplesheet.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# v1.0 (11/25/2022)
+#
+# Adds filepaths for SRA FastQs automatically downloaded by Phoenix
+# Auto mapping of filepath for user 
+# ##Usage: >python scaffolds_samplesheet.py <FILENAME>
+# Written by Maria Diaz (lex0@cdc.gov)
+
+
