809e43e462b8bdb787449bd4d5e5728118e858eb
Maria Diaz <69120361+MzDiaz824@users.noreply.github.com>
2022-11-25 14:34:37 -0500

    args added

--- bin/scaffolds_samplesheet.py
+++ bin/scaffolds_samplesheet.py
@@ -6,4 +6,12 @@
 # ##Usage: >python scaffolds_samplesheet.py <FILENAME>
 # Written by Maria Diaz (lex0@cdc.gov)
 
+import os 
+import argparse
+import pandas as pd
+
+def parseArgs(args=None):
+    parser = argparse.ArgumentParser(description='Script to generate a complete PhoeNix scaffolds samplesheet')
+    parser.add_argument('file', nargs=argparse.REMAINDER)
+    return parser.parse_args()
 
